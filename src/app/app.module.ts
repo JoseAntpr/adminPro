fbc5ba82006f1e1c5e6e50712e5fdc49549e134b
José Antonio Palacios Ramírez <joseantpalaciosramirez@gmail.com>
2018-07-10 13:02:07 +0200

    ADD: reactive forms in register pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 // Modulos
 import { PagesModule } from './pages/pages.module';
@@ -33,7 +33,8 @@ import { ServiceModule } from './services/service.module';
     PagesModule,
     RoutingModule,
     FormsModule,
-    ServiceModule
+    ServiceModule,
+    ReactiveFormsModule
   ],
   providers: [
   ],
--- src/app/login/sign-up.component.ts
+++ src/app/login/sign-up.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+import swal from 'sweetalert';
+
+declare function init_plugins();
 
 @Component({
   selector: 'app-sign-up',
@@ -7,9 +12,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpComponent implements OnInit {
 
+  form: FormGroup;
+
   constructor() { }
 
   ngOnInit() {
+    init_plugins();
+
+    this.form = new FormGroup({
+      name: new FormControl( null, Validators.required ),
+      email: new FormControl( null, [Validators.required, Validators.email] ),
+      password: new FormControl( null, Validators.required ),
+      password2: new FormControl( null, Validators.required ),
+      conditions: new FormControl()
+    }, {validators: this.sameField('password', 'password2')});
+  }
+
+  sameField( field1: string, field2: string) {
+    return (group: FormGroup) => {
+      const pass1 = group.controls[field1].value;
+      const pass2 = group.controls[field2].value;
+
+      if (pass1 === pass2 ) {
+        return null;
+      }
+      return {
+        areEqual: true
+      };
+    };
+  }
+
+  register() {
+    if ( this.form.invalid) {
+      return;
+    }
+
+    if ( !this.form.value.conditions ) {
+      swal('Importante', 'Debe aceptar las conciones', 'warning');
+      return;
+    }
   }
 
 }
